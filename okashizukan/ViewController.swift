0181010e58b72a759a37e71552fe65514f27b6ed
Takeshi Kanazawa <oseans@hotmail.co.jp>
2020-04-07 21:40:44 +0900

    first commit

---
+++ okashizukan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  okashizukan
+//
+//  Created by 金澤武士 on 2020/04/07.
+//  Copyright © 2020 tk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
